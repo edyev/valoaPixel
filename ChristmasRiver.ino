e5a9debd89a5052422f0279fe76e5823ab37a547
Eddie Vazquez <lalo_psp@hotmail.com>
2024-01-05 19:55:29 -0600

    Update colours to use bicolours

--- ChristmasRiver.ino
+++ ChristmasRiver.ino
@@ -20,26 +20,26 @@ void setup() {
   Serial.begin(9600);
     colours = (riverPixels::colourPair*) malloc(sizeof(riverPixels::colourPair) * NUMBER_PAIRS );
      
-    colours[0].c1 = {0, 0, 0xa};
-    colours[0].c2 = {0x1, 0x1, 0xff};
-    colours[1].c1 = {0, 0xa, 0xa};
+    colours[0].c1 = {0, 0, 0xff};
+    colours[0].c2 = {0xff, 0xff, 0};
+    colours[1].c1 = {0xff, 0, 0xff};
     colours[1].c2 = {0, 0xff, 0xff};    
-    colours[2].c1 = {0, 0xc, 0x2};
-    colours[2].c2 = {0, 0xff, 0xa};
-    colours[3].c1 = {30,5,0};
-    colours[3].c2 = {255, 40, 0};
-    colours[4].c1 = {0x10,0,0};
-    colours[4].c2 = {0xff, 0, 0};
-    colours[5].c1 = {0,0x10,0};
-    colours[5].c2 = {0, 0xff, 0};
-    colours[6].c1 = {0x5,0x5,0x5};
-    colours[6].c2 = {0xf0, 0xf0, 0xf};
-    colours[7].c1 = {0x10,0,0x10};
-    colours[7].c2 = {0xff, 0, 0xff};
-    colours[8].c1 = {0x50,0x50,0};
-    colours[8].c2 = {0xff, 0xff,0};
+    colours[2].c1 = {0xff, 0xff, 0xff};
+    colours[2].c2 = {0xff, 0, 0};
+    colours[3].c1 = {0x2e,0xcc,0x71}; 
+    colours[3].c2 = {0x89, 0x5c, 0xde};
+    colours[4].c1 = {0xf9,0xe7,0x9f}; 
+    colours[4].c2 = {0xf5, 0xcb, 0xa7}; 
+    colours[5].c1 = {0xaf,0x7a,0xc4};
+    colours[5].c2 = {0x29, 0x80, 0xb9}; 
+    colours[6].c1 = {0x2e,0xcc,0x71}; 
+    colours[6].c2 = {0xf4, 0xd0, 0x3f}; 
+    colours[7].c1 = {0x5d,0x6d,0x7e}; 
+    colours[7].c2 = {0xd1, 0xf2, 0xeb};  
+    colours[8].c1 = {0x99,0x33,0xff};
+    colours[8].c2 = {0xff, 0xff,0xff};
 
-  pixels = new riverPixels(2, NUMBER_PAIRS, colours, 500u );
+  pixels = new riverPixels(8, NUMBER_PAIRS, colours, 500u );
   pixels->setPixelsLength(NUM_LEDS);
   pixels->hardware_init();
 
@@ -47,7 +47,5 @@ void setup() {
 
 void loop() {
   pixels->run();
-  potVal = analogRead(potPin);
-  potVal = map(potVal, 0, 1023, 10, 100);
-  delay(random(18, 180));
+  delay(100);
 }
